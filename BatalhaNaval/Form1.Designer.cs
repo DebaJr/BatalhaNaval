6202f0411bc2e7be5bcd2fd8f55810478a32ebd3
José Roberto Debastiani Júnior <debastianijunior@hotmail.com>
2024-11-17 19:48:11 -0300

    Adicionar arquivos de projeto.

---
+++ BatalhaNaval/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace BatalhaNaval
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPlayerPhase = new System.Windows.Forms.Label();
+            this.txtPlayerScore = new System.Windows.Forms.Label();
+            this.txtPlayerScoreLabel = new System.Windows.Forms.Label();
+            this.txtCPUScore = new System.Windows.Forms.Label();
+            this.txtCPUScoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPlayerPhase
+            // 
+            this.txtPlayerPhase.AutoSize = true;
+            this.txtPlayerPhase.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlayerPhase.Location = new System.Drawing.Point(128, 416);
+            this.txtPlayerPhase.Name = "txtPlayerPhase";
+            this.txtPlayerPhase.Size = new System.Drawing.Size(208, 24);
+            this.txtPlayerPhase.TabIndex = 0;
+            this.txtPlayerPhase.Text = "Escolha suas posições!";
+            // 
+            // txtPlayerScore
+            // 
+            this.txtPlayerScore.AutoSize = true;
+            this.txtPlayerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlayerScore.Location = new System.Drawing.Point(220, 16);
+            this.txtPlayerScore.Name = "txtPlayerScore";
+            this.txtPlayerScore.Size = new System.Drawing.Size(20, 24);
+            this.txtPlayerScore.TabIndex = 0;
+            this.txtPlayerScore.Text = "0";
+            // 
+            // txtPlayerScoreLabel
+            // 
+            this.txtPlayerScoreLabel.AutoSize = true;
+            this.txtPlayerScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlayerScoreLabel.Location = new System.Drawing.Point(147, 16);
+            this.txtPlayerScoreLabel.Name = "txtPlayerScoreLabel";
+            this.txtPlayerScoreLabel.Size = new System.Drawing.Size(67, 24);
+            this.txtPlayerScoreLabel.TabIndex = 0;
+            this.txtPlayerScoreLabel.Text = "Player:";
+            // 
+            // txtCPUScore
+            // 
+            this.txtCPUScore.AutoSize = true;
+            this.txtCPUScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCPUScore.Location = new System.Drawing.Point(616, 19);
+            this.txtCPUScore.Name = "txtCPUScore";
+            this.txtCPUScore.Size = new System.Drawing.Size(20, 24);
+            this.txtCPUScore.TabIndex = 0;
+            this.txtCPUScore.Text = "0";
+            // 
+            // txtCPUScoreLabel
+            // 
+            this.txtCPUScoreLabel.AutoSize = true;
+            this.txtCPUScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCPUScoreLabel.Location = new System.Drawing.Point(543, 19);
+            this.txtCPUScoreLabel.Name = "txtCPUScoreLabel";
+            this.txtCPUScoreLabel.Size = new System.Drawing.Size(53, 24);
+            this.txtCPUScoreLabel.TabIndex = 0;
+            this.txtCPUScoreLabel.Text = "CPU:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.txtCPUScoreLabel);
+            this.Controls.Add(this.txtCPUScore);
+            this.Controls.Add(this.txtPlayerScoreLabel);
+            this.Controls.Add(this.txtPlayerScore);
+            this.Controls.Add(this.txtPlayerPhase);
+            this.Name = "Form1";
+            this.Text = "BatalhaNaval";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtPlayerPhase;
+        private System.Windows.Forms.Label txtPlayerScore;
+        private System.Windows.Forms.Label txtPlayerScoreLabel;
+        private System.Windows.Forms.Label txtCPUScore;
+        private System.Windows.Forms.Label txtCPUScoreLabel;
+    }
+}
+
---
+++ BatalhaNaval/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Web;
+
+namespace BatalhaNaval
+{
+    public partial class Form1 : Form
+    {
+        char[] letterPositions = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+        List<Button> enemyShipPositions = new List<Button>();
+        List<Button> playerShipPositions = new List<Button>();
+        List<Button> playerBoard = new List<Button>();
+        List<Button> enemyBoard = new List<Button>();
+
+        bool playerChoosingPositions = false;
+
+        int playerScore = 0;
+        int cpuScore = 0;
+
+        int tamanhoTabuleiro = 10;
+
+        Random randGen = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+
+            CreatePlayerBoard(playerBoard, 40, 50);
+            CreateEnemyBoard(enemyBoard, 400, 50);
+
+            ResetGame();
+        }
+
+        private void ResetGame()
+        {
+            enemyShipPositions.Clear();
+            playerShipPositions.Clear();
+
+            foreach (Button btn in playerBoard)
+            {
+                btn.Enabled = true;
+                btn.Tag = null;
+                btn.BackColor = Color.White;
+                btn.Text = btn.Name.ToUpper();
+            }
+
+            foreach (Button btn in enemyBoard)
+            {
+                btn.Enabled = false;
+                btn.BackColor = Color.White;
+                btn.Text = btn.Name.ToUpper();
+            }
+
+            enemyPositionPicker();
+            txtPlayerPhase.Text = "Escolha suas posições (" + (10 - playerShipPositions.Count) + ")!";
+            playerChoosingPositions = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playerBoard_Click(object sender, EventArgs e)
+        {
+            if (playerChoosingPositions && playerShipPositions.Count < tamanhoTabuleiro)
+            {
+                Button clickedButton = (Button)sender;
+
+                playerShipPositions.Add(clickedButton);
+                clickedButton.BackColor = Color.Yellow;
+                clickedButton.Enabled = false;
+
+                if (playerShipPositions.Count < tamanhoTabuleiro)
+                {
+                    txtPlayerPhase.Text = "Escolha suas posições (" + (10 - playerShipPositions.Count) + ")!";
+                }
+                else
+                {
+                    txtPlayerPhase.Text = "Escolha uma posição no tabuleiro inimigo!";
+                    foreach (Button btn in playerBoard)
+                    {
+                        btn.Enabled = false;
+                    }
+
+                    foreach (Button btn in enemyBoard)
+                    {
+                        btn.Enabled = true;
+                    }
+                    playerChoosingPositions = false;
+                }
+            }
+        }
+
+        private void enemyBoard_Click(object sender, EventArgs e)
+        {
+            if (!playerChoosingPositions)
+            {
+                Button clickedButton = (Button)sender;
+
+                if (enemyShipPositions.Contains(clickedButton))
+                {
+                    clickedButton.BackColor = Color.Red;
+                    clickedButton.Enabled = false;
+                    enemyShipPositions.Remove(clickedButton);
+                    MessageBox.Show("Acertou!");
+                    if (enemyShipPositions.Count < 1)
+                    {
+                        MessageBox.Show("Parabéns, você venceu!");
+                        playerScore++;
+                        txtPlayerScore.Text = playerScore.ToString();
+                        ResetGame();
+                    }
+                    else
+                    {
+                        EnemyAttack();
+                    }
+                }
+                else
+                {
+                    clickedButton.BackColor = (Color)Color.DarkBlue;
+                    clickedButton.Enabled = false;
+                    MessageBox.Show("Água!");
+                    EnemyAttack();
+                }
+
+
+            }
+        }
+
+        private void EnemyAttack()
+        {
+            int index = randGen.Next(playerBoard.Count);
+
+            while (playerBoard[index].Tag == "shot")
+            {
+                index = randGen.Next(playerBoard.Count);
+            }
+
+            MessageBox.Show("O ataque da CPU vai em: " + playerBoard[index].Text);
+            if (playerShipPositions.Contains(playerBoard[index]))
+            {
+                playerBoard[index].BackColor = Color.Red;
+                playerBoard[index].Tag = "shot";
+                playerShipPositions.Remove(playerBoard[index]);
+                MessageBox.Show("Te acertei!");
+                if (playerShipPositions.Count < 1)
+                {
+                    MessageBox.Show("Você perdeu...");
+                    cpuScore++;
+                    txtCPUScore.Text = cpuScore.ToString();
+                    ResetGame();
+                }
+
+            }
+            else
+            {
+                playerBoard[index].BackColor = Color.DarkBlue;
+                playerBoard[index].Tag = "shot";
+                MessageBox.Show("Água... sua vez.");
+            }
+        }
+
+        private void CreatePlayerBoard(List<Button> boardToCreate, int startPositionX, int startPositionY)
+        {
+            for (int i = 0; i < tamanhoTabuleiro; i++)
+            {
+                for (int j = 0; j < tamanhoTabuleiro; j++)
+                {
+                    Button newButton = new Button();
+                    this.Controls.Add(newButton);
+                    newButton.Name = letterPositions[i].ToString() + (j + 1).ToString();
+                    newButton.Text = newButton.Name.ToUpper();
+                    newButton.Location = new Point(i * 35 + startPositionX, j * 35 + startPositionY);
+                    newButton.Size = new Size(35, 35);
+                    newButton.Click += playerBoard_Click;
+                    boardToCreate.Add(newButton);
+                }
+            }
+        }
+
+        private void CreateEnemyBoard(List<Button> boardToCreate, int startPositionX, int startPositionY)
+        {
+            for (int i = 0; i < tamanhoTabuleiro; i++)
+            {
+                for (int j = 0; j < tamanhoTabuleiro; j++)
+                {
+                    Button newButton = new Button();
+                    this.Controls.Add(newButton);
+                    newButton.Name = letterPositions[i].ToString() + (j + 1).ToString();
+                    newButton.Text = newButton.Name.ToUpper();
+                    newButton.Location = new Point(i * 35 + startPositionX, j * 35 + startPositionY);
+                    newButton.Size = new Size(35, 35);
+                    newButton.Click += enemyBoard_Click;
+                    boardToCreate.Add(newButton);
+                }
+            }
+        }
+
+        private void enemyPositionPicker()
+        {
+            while (enemyShipPositions.Count < tamanhoTabuleiro)
+            {
+                int index = randGen.Next(enemyBoard.Count);
+                if (!enemyShipPositions.Contains(enemyBoard[index]))
+                {
+                    enemyShipPositions.Add(enemyBoard[index]);
+                    Debug.WriteLine(enemyBoard[index].Text);
+                }
+            }
+
+        }
+    }
+}
